cec20aca41c31a29efa9bd04669adee53421c663
Arne Emil Buchter <5651612@RTS-365.dk>
2020-09-29 14:12:10 +0200

    addOmos

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,22 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
     let sections = 'http://localhost:4000/api/v1/adoptsections'
+    let abouts = 'http://localhost:4000/api/v1/abouts'
+    let volunteers = 'http://localhost:4000/api/v1/volunteers'
     fetch(`${sections}`,{
         'method': 'GET'
 
     }) //fetch
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         data.forEach(element => {
             if(element.asset.id === 11){
                 document.querySelector('.header').style.backgroundImage = `url('${element.asset.url}')`
                 document.querySelector('.header__title').innerText = element.title
                 document.querySelector('.header__text').innerText = element.content
-                console.log(element)
             }
         });
     })
 
+    fetch(`${abouts}`,{
+        'method': 'GET'
+
+    }) //fetch
+    .then(response => response.json())
+    .then(data => {
+        let template = document.querySelector('.about__template');
+        let list = document.querySelector('.about');
+        data.forEach(element => {
+            console.log(element) 
+            let clone = template.content.cloneNode(true);
+            clone.querySelector('.about__heading').innerText = element.title
+            clone.querySelector('.about__text').innerText = element.content
+            list.appendChild(clone)
+        });
+    })
 
 
+    fetch(`${volunteers}`,{
+        'method': 'GET'
+
+    }) //fetch
+    .then(response => response.json())
+    .then(data => {
+        let template = document.querySelector('.volunteers__template');
+        let list = document.querySelector('.volunteers'); 
+        data.forEach(element => {
+            console.log(element) 
+            let clone = template.content.cloneNode(true);
+            clone.querySelector('.volunteers__heading').innerText = element.title
+            clone.querySelector('.volunteers__text').innerText = element.content
+            list.appendChild(clone)
+        });
+    })
 })
